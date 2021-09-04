4abfb5e0e04d83fb149e8ddb164363e7d9b0396c
brunobaezmed <79769690+brunobaezmed@users.noreply.github.com>
2021-09-03 21:09:25 -0400

    Add files via upload

--- algKW/Sone/src/test/java/com/S1/Sone/SoneApplicationTests.java
+++ algKW/Sone/src/test/java/com/S1/Sone/SoneApplicationTests.java
@@ -1,24 +1,22 @@
 package com.S1.Sone;
-
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.S1.Sone.UserTimeService.mariadbCrud;
+
 class SoneApplicationTests {
-	
+
 		@Test
 		void NtestDataaccess() {	
 	
 	}
-		@Test
-		public static void main(String args[]) {
-		GregorianCalendar calendar=new GregorianCalendar();
-		int t=calendar.getFirstDayOfWeek();
-		t=calendar.getWeekYear();
-		t=calendar.getFirstDayOfWeek();
-		Date Time=calendar.getTime();
-		TimeZone timezone=calendar.getTimeZone();
-			}
-
+		
 }
+
+/*
+
+SELECT usersaccess.name , usersaccess.Lastname , userstime.age
+FROM usersaccess,userstime
+WHERE usersaccess.id = userstime.id;
+*/
