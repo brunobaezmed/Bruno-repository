714497bb47072fca056d99cf7fbf67b907be0ce4
brunobaezmed <79769690+brunobaezmed@users.noreply.github.com>
2021-09-03 21:27:18 -0400

    Update mariadbCrud.java

--- algKW/Sone/src/main/java/com/S1/Sone/UserTimeService/mariadbCrud.java
+++ algKW/Sone/src/main/java/com/S1/Sone/UserTimeService/mariadbCrud.java
@@ -4,6 +4,6 @@ package com.S1.Sone.UserTimeService;
 import org.springframework.data.repository.CrudRepository;
 import javax.management.Query;
 import com.S1.Sone.models.Userstime;
-public interface mariadbCrud extends CrudRepository<Userstime,Long>  {
+public interface MariadbCrud extends CrudRepository<Userstime,Long>  {
 	@Query("SELECT usersaccess.name  usersaccess.Lastname , userstime.age FROM usersaccess,userstime WHERE usersaccess.id = userstime.id");
 }
