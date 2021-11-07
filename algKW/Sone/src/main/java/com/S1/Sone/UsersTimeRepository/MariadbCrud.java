57015fc1ea104b70db7aef9d92036c635997f9cc
brunobaezmed <79769690+brunobaezmed@users.noreply.github.com>
2021-11-07 17:06:56 -0500

    Delete algKW directory

--- algKW/Sone/src/main/java/com/S1/Sone/UsersTimeRepository/MariadbCrud.java
+++
@@ -1,9 +0,0 @@
-package com.S1.Sone.UsersTimeRepository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.S1.Sone.models.Userstime;
-
-public interface MariadbCrud extends CrudRepository<Userstime,Long> {
-
-}
--- algKW/tmain.java
+++
@@ -1,8 +0,0 @@
-public class tmain {
-
-	static BinarySearch s=new BinarySearch();
-	public static void main(String[] args) {
-		
-	}
-
-}
