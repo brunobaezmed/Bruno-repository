c7b49b5f6f9e945a8822ca1e4c73aec2c0a91fc8
bruno <brunomatias33@gmail.com>
2021-11-10 19:09:07 -0300

    examples1

--- window.cpp
+++ window.cpp
@@ -139,13 +139,13 @@ void start_()
      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
      int width, height, channel;
      stbi_set_flip_vertically_on_load(true);
-     unsigned char *data = stbi_load("/home/bruno/Downloads/container.png", &width, &height, &channel, 0);
+     unsigned char *data = stbi_load("/home/bruno/Downloads/container.jpg", &width, &height, &channel, 0);
 
      glTexImage2D(GL_TEXTURE_2D,
                   0,
                   GL_RGB,
-                  128,
-                  128,
+                  width,
+                  height,
                   0,
                   GL_RGB,
                   GL_UNSIGNED_BYTE,
